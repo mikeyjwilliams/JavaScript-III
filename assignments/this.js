19227c589a6e6d7de8cd746001205ef590249dbd
mikey <manicdeisgner@gmail.com>
2019-04-23 09:43:54 -0400

    answer(this) problem 2 answer to this problem 2

--- assignments/this.js
+++ assignments/this.js
@@ -2,9 +2,9 @@
 * in your own words. explain the four principle for the "this" keyword below.
 *
 * 1. this globally calls the window/global object binding or all of javascript and its functions.
-* 2. 
+* 2. implicit binding, a function is called and the object prior to `.function()` call would then be considered this.
 * 3. 
-* 4. 
+* 4.
 *
 * write out a code example of each explanation above
 */
