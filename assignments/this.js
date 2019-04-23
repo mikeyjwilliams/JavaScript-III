ef7b30ba1f0834a2a8fc68792780fc9f0da5517e
mikey <manicdesigner@gmail.com>
2019-04-23 13:55:46 -0400

    rewrite(this) rewrote answer 2 changed some of answer 2 for this

--- assignments/this.js
+++ assignments/this.js
@@ -1,13 +1,12 @@
 /* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
-*
-* 1. this globally calls the window/global object binding or all of javascript and its functions.
-* 2. implicit binding, a function is called and the object prior to `.function()` call would then be considered this.
-* 3. when a constructor function is used, points to the instance made and returned by said constructor function.
-* 4. call or apply methods used. this is defined.
-*
-* write out a code example of each explanation above
-*/
+ * in your own words. explain the four principle for the "this" keyword below.
+ * 1. this globally calls the window/global object binding or all of javascript and its functions.
+ * 2. implicit binding, this binds automatically to the object it is called inside and the object prior to `.function()` call would then be considered this.
+ * 3. when a constructor function is used, points to the instance made and returned by said constructor function.
+ * 4. call or apply methods used. this is defined.
+ *
+ * write out a code example of each explanation above
+ */
 
 // Principle 1
 
@@ -21,13 +20,16 @@ function greetingWindow(name) {
 
 // code example for Implicit Binding
 const bandFunc = bandNames => {
-    bandNames.favoriteBand = function() {
+    bandNames.favoriteBand = function () {
         console.log(`My favorite band is ${this.favBand}.`);
         console.log(`While your favorite band is ${this.name}.`);
         console.log(this);
     };
 }
-const bands = {favBand: 'Thrice', name: 'Something Corporate'};
+const bands = {
+    favBand: 'Thrice',
+    name: 'Something Corporate'
+};
 bandFunc(bands);
 //bands.favoriteBand();
 
