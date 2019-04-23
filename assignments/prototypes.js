b78b0e1a26f1472ef7df77edf13b6ad02405aa38
mikey <manicdesigner@gmail.com>
2019-04-23 18:19:25 -0400

    feat(prototypes) characterStats .call inherit GameObject props into CharacterStats

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -31,6 +31,7 @@ GameObject.prototype.destroy = function () {
   * should inherit destroy() from GameObject's prototype
 */
 function CharacterStats(stats) {
+  GameObject.call(this, stats); // inheriting GameObject properties.
   this.healthPoints = stats.healthPoints;
 }
 // inherit destroy() from GameObject prototype
